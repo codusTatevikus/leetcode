a305a3e105def50985ba1944efc0b0ec9ed75cac
stratechie <247036779+codusTatevikus@users.noreply.github.com>
2026-01-02 23:36:09 +0400

    1431

---
+++ easy/problem_1431.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result{};
+
+        for (size_t i = 0; i < candies.size(); ++i)
+        {
+            if (candies[i] + extraCandies >= *max_element(candies.begin(), candies.end()))
+            {
+                result.push_back(true);
+            }
+            else
+            {
+                result.push_back(false);
+            }
+        }
+
+        return result;
+    }
+};
