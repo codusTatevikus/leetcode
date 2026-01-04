8a7132d2fc9669bb6ae9335c1031be98740e794e
stratechie <247036779+codusTatevikus@users.noreply.github.com>
2026-01-04 15:57:38 +0400

    1480 update

--- easy/problem_1480.cpp
+++ easy/problem_1480.cpp
@@ -3,10 +3,7 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         for (size_t i = 1; i < nums.size(); ++i)
         {
-            for (size_t j = i - 1; j < i; ++j)
-            {
-                nums[i] += nums[j];
-            }
+            nums[i] += nums[i - 1];
         }
         return nums;
     }
