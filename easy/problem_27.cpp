cad1a652846b61398c950aa2309ab0e03bf10c60
codusTatevikus <247036779+codusTatevikus@users.noreply.github.com>
2026-01-12 22:29:02 +0400

    27 2

--- easy/problem_27.cpp
+++ easy/problem_27.cpp
@@ -16,3 +16,23 @@ public:
         return nums.size() - count;
     }
 };
+
+#2
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count{};
+
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != val)
+            {
+                nums[count] = nums[i];
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
