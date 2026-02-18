8e866ade09c680bc2591fa803f3515b92c1c4aaf
codusTatevikus <247036779+codusTatevikus@users.noreply.github.com>
2026-02-18 22:19:09 +0400

    1688

---
+++ easy/problem_1688.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        answer = 0
+        while n > 1:
+            if n % 2 == 0:
+                answer += n/2
+                n = n / 2
+            else:
+                answer += (n - 1) / 2
+                n = (n - 1) / 2 + 1
+        return int(answer)
