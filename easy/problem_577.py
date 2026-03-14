1aae23e89d0dc68714b17e951313e2dce1c25532
codusTatevikus <247036779+codusTatevikus@users.noreply.github.com>
2026-03-14 23:17:09 +0400

    577

---
+++ easy/problem_577.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        employee
+        .merge(bonus, on="empId", how="left")
+    )
+
+    result = result[
+        (result["bonus"] < 1000) | (result["bonus"].isna())
+    ][["name", "bonus"]]
+
+    return result.reset_index(drop=True)
