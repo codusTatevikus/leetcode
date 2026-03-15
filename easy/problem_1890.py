c3d20546b6183dfaf64acf7f87929bcb4adb9b13
codusTatevikus <247036779+codusTatevikus@users.noreply.github.com>
2026-03-15 22:00:02 +0400

    1890

---
+++ easy/problem_1890.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def latest_login(logins: pd.DataFrame) -> pd.DataFrame:
+    logins_2020 = logins[logins["time_stamp"].dt.year == 2020]
+
+    result = (
+        logins_2020
+        .groupby("user_id", as_index=False)["time_stamp"]
+        .max()
+        .rename(columns={"time_stamp": "last_stamp"})
+    )
+
+    return result
