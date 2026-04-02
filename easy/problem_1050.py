e4a4e8e83ac2b2f6c65dca06717c7238396a1fd6
codusTatevikus <247036779+codusTatevikus@users.noreply.github.com>
2026-04-02 22:46:53 +0400

    1050

---
+++ easy/problem_1050.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        actor_director
+        .groupby(["actor_id", "director_id"])
+        .size()
+        .reset_index(name="count")
+    )
+
+    result = result[result["count"] >= 3][["actor_id", "director_id"]]
+
+    return result.reset_index(drop=True)
