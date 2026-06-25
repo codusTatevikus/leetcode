121cc58b0623771e6a9787d3cd909754657573fd
codusTatevikus <tatev.srapivonyan@gmail.com>
2026-06-25 18:23:14 +0400

    1587bsql version

---
+++ easy/problem_1587.sql
@@ -0,0 +1,5 @@
+SELECT Users.name, SUM(Transactions.amount) AS balance
+FROM Users
+INNER JOIN Transactions ON Users.account = Transactions.account
+GROUP BY Users.account, Users.name
+HAVING SUM(Transactions.amount) > 10000;
